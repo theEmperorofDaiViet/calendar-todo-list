42d0988f4fdc8a4a6423e43a5c64ef91af82028f
theEmperorofDaiViet <Khiet.To.Development@gmail.com>
2023-07-22 23:32:48 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <calendar-week/>
+    <calendar-entry/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalendarWeek from './components/CalendarWeek.vue';
+import CalendarEntry from './components/CalendarEntry.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    "calendar-week": CalendarWeek,
+    "calendar-entry": CalendarEntry
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
 }
 </style>
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="calendar-week" class="container">
+        <div class="columns is-mobile">
+            <calendar-day v-for="day in sharedState.data" :key="day.id" :day="day"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from '../store.js'
+import CalendarDay from './CalendarDay.vue'
+
+export default {
+    name: 'CalendarWeek',
+    data() {
+        return {
+            sharedState: store.state
+        };
+    },
+    components: {
+        "calendar-day": CalendarDay
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#calendar-week {
+    margin-bottom: 50px;
+    .column {
+        padding: 0 0 0 0;
+    }
+}
+</style>
